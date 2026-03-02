843304cb9e486632fc02f010c0338683c75d5a99
Devshree <sharmadevshree333@gmail.com>
2026-03-02 19:57:03 +0530

    Problem 35: Search Insert Position.cpp

---
+++ Arrays/Problem 35: Search Insert Position.cpp
@@ -0,0 +1,26 @@
+class Solution {
+public:
+    int searchInsert(vector<int>& nums, int target) {
+        int n=nums.size();
+
+        for(int i=0;i<n;i++)
+        {
+            if(nums[i]==target)
+            {
+                return i;
+            }
+            else
+            {
+                if(nums[i]>target)
+                {
+                    return i;
+                }
+                if(nums[n-1]<target)
+                {
+                    return n;
+                }
+            }
+        }
+        return -1;
+    }
+};
